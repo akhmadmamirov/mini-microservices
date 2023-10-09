171a118a3fb6b4f7771e2a011444df0982861565
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-10-08 21:18:06 -0400

    fixing event-bus starter code

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,23 +1,37 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const axios = require("axios")
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
+app.post("/events", async (req, res) => {
+    const event = req.body;
 
-axios.post("/events", (req,res) => {
-    const event = req.body
+    try {
+        await axios.post('http://localhost:4000/events', event);
+        console.log("Posted to 4000");
+    } catch (err) {
+        console.error("Error posting to 4000", err);
+    }
 
-    axios.post('http://localhost:4000/events', event).catch(err => console.log(err));
-    axios.post('http://localhost:4001/events', event).catch(err => console.log(err));
-    axios.post('http://localhost:4002/events', event).catch(err => console.log(err));
+    try {
+        await axios.post('http://localhost:4001/events', event);
+        console.log("Posted to 4001");
+    } catch (err) {
+        console.error("Error posting to 4001", err);
+    }
 
-    res.status(201).json({"status": "OK"})
-})
+    try {
+        await axios.post('http://localhost:4002/events', event);
+        console.log("Posted to 4002");
+    } catch (err) {
+        console.error("Error posting to 4002", err);
+    }
 
-app.listen(4005,() => {
-    console.log("Listening on Port 4005")
-})
+    res.status(201).json({"status": "OK"});
+});
 
-
+app.listen(4005, () => {
+    console.log("Listening on Port 4005");
+});
