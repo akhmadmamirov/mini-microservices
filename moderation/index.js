381605b3fe238c0ed08a81ebb1862a6e967c914c
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-10-21 21:49:58 -0400

    Adding moderation service

---
+++ moderation/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.post('/events', (req, res) => {
+
+    
+})
+
+
+
+
+app.listen(4003, (req, res) => {
+    console.log('Listening on port 4003')
+})
